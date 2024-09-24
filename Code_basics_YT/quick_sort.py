f0a2d97ca9ba9b850ebee05ffb5d22410a1629fc
majkmikeamai <125311513+majkmikeamai@users.noreply.github.com>
2024-09-24 18:16:57 +0200

    quick sort tutorial finished

---
+++ Code_basics_YT/quick_sort.py
@@ -0,0 +1,55 @@
+def lomuto_partition(elements, start, end):
+    pivot = elements[end]
+
+    i = start - 1
+
+    for j in range(start, end):
+        if elements[j] <= pivot:
+            i += 1
+            elements[i], elements[j] = elements[j], elements[i]
+
+    elements[i + 1], elements[end] = elements[end], elements[i + 1]
+    print(elements)
+    print(i + 1)
+    return i + 1
+
+
+def hoare_partition(elements, start, end):
+    pivot = elements[start]
+
+    i = start
+    j = end
+    while i < j:
+        if elements[i] <= pivot:
+            i += 1
+
+        elif elements[j] > pivot:
+            j -= 1
+        else:
+            elements[j], elements[i] = elements[i], elements[j]
+            i += 1
+            j -= 1
+    elements[start], elements[j] = elements[j], elements[start]
+    print(i, j)
+    print(elements)
+
+    return j
+
+
+def quick_sort(elements, start, end):
+    if len(elements) == 1:
+        return
+    if start < end:
+        p_idx = lomuto_partition(elements, start, end)
+        print("left")
+        quick_sort(elements, start, p_idx - 1)
+        print("left has been performed")
+        print("right")
+        quick_sort(elements, p_idx + 1, end)
+        print("right has been performed")
+    return elements
+
+
+if __name__ == "__main__":
+    elements = [11, 9, 29, 7, 2, 15, 28]
+    qs = quick_sort(elements, 0, len(elements) - 1)
